4bf608523ed420ccf303797523a331fa694c3239
AragornLoki <420165259@qq.com>
2020-09-03 21:41:10 +0800

    Signed-off-by: AragornLoki <420165259@qq.com>

--- myweb_server/src/main/java/tk/lokiShelter/server/service/BrandService.java
+++
@@ -1,9 +0,0 @@
-package tk.lokiShelter.server.service;
-
-import tk.lokiShelter.db.domain.LsBrand;
-
-import java.util.List;
-
-public interface BrandService {
-    List<LsBrand> listBrand(int pageNum, int pageSize);
-}
